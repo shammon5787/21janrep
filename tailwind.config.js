581ba9af4443290e4744518e347a0cae37167f8c
shammon5787 <sadam.bsit@gmail.com>
2025-01-28 17:06:55 +0500

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}", // Include all source files
+//   ],
+//   theme: {
+//     extend: {}, // Customize your theme here
+//   },
+//   plugins: [],
+// };
